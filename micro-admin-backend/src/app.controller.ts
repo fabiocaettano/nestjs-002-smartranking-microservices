cd84614323beebdb6359986d71e7333c0ecd4a20
fabiocaettano <fabio.caettano74@gmail.com>
2023-04-02 18:45:35 +0000

    configurar mongodb

--- micro-admin-backend/src/app.controller.ts
+++ micro-admin-backend/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
+import { EventPattern, Payload } from '@nestjs/microservices';
+import { Categoria } from './interfaces/categorias/categoria.interface';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  logger = new Logger(AppController.name);
+
+  @EventPattern('criar-categoria')
+  async criarCategoria(
+    @Payload() categoria: Categoria
+  ){
+    this.logger.log(`categoira: ${JSON.stringify(categoria)}`);
   }
+  
 }
