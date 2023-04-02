c1ba0981093220797d6315a009f931c7652cf33d
fabiocaettano <fabio.caettano74@gmail.com>
2023-04-02 11:18:49 +0000

    implementado criar-categoria-dto e eventos.interfaces

---
+++ api-gateway/src/dtos/criar-categoria-dto.ts
@@ -0,0 +1,17 @@
+import { ArrayMinSize, IsArray, IsNotEmpty, IsString } from "class-validator";
+import { Eventos } from "src/entity/interfaces/eventos.interface";
+
+export class CriarCategoriaDto{
+
+    @IsString()
+    @IsNotEmpty()
+    readonly categoria: string;
+
+    @IsString()
+    @IsNotEmpty()
+    descricao: string
+
+    @IsArray()
+    @ArrayMinSize(1)
+    eventos: Array<Eventos>
+}
