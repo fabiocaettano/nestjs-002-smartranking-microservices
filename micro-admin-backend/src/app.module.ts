ed23bbc480b4c5a958758678249ff180605e06ac
fabiocaettano <fabio.caettano74@gmail.com>
2023-04-02 17:48:15 +0000

    configurado o app.module para acessar o banco de dados mongodb

--- micro-admin-backend/src/app.module.ts
+++ micro-admin-backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriaSchema } from './interfaces/categorias/categoria.schema';
+import { JogadorSchema } from './interfaces/jogadores/jogador.schema';
+require('dotenv').config({ path: '.env' })
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forFeature([
+      {name: 'Categoria', schema: CategoriaSchema},
+      {name: 'Jogador', schema: JogadorSchema}
+    ]
+    )],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
