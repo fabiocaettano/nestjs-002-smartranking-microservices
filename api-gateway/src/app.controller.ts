b641e264fc68ea360e7eaef68fcb00b670cc7698
fabiocaettano <fabio.caettano74@gmail.com>
2023-04-02 11:38:03 +0000

    implementação do emit enviando o tópico criar categoria

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,6 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller , Logger } from '@nestjs/common';
+import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {  
 
+    private logger = new Logger(AppController.name);
+
+    private clientAdminBackend: ClientProxy;
+
+    constructor(){
+        this.clientAdminBackend = ClientProxyFactory.create({
+            transport: Transport.RMQ,
+            options: {
+                urls: ['amqp://user:nomeDoUser@IP/smartranking'],
+                queue: 'admin-backend'
+            }
+        })
+
+    }
 }
