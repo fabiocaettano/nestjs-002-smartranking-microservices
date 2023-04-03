0743190d0c122a18cdbe1e2dadf74a04403dd706
fabiocaettano <fabio.caettano74@gmail.com>
2023-04-03 02:13:32 +0000

    configuração mongoose

--- micro-admin-backend/src/app.module.ts
+++ micro-admin-backend/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CategoriaSchema } from './interfaces/categorias/categoria.schema';
 import { JogadorSchema } from './interfaces/jogadores/jogador.schema';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 require('dotenv').config({ path: '.env' })
 
 @Module({
@@ -12,7 +14,7 @@ require('dotenv').config({ path: '.env' })
       {name: 'Jogador', schema: JogadorSchema}
     ]
     )],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],  
+  providers: [AppService],
 })
 export class AppModule {}
