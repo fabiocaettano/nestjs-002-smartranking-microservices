94df33ea49d51f264bc720d44f8670d4f1db8189
fabiocaettano <fabiocaettano74@gmail.com>
2023-04-07 22:46:07 +0000

    aula 47 implementado atualizarCategoria

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller , Get, Logger, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller , Get, Logger, Param, Post, Put, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
 import { CriarCategoriaDto } from './dtos/criar-categoria-dto';
 import { Observable } from 'rxjs';
+import { AtualizarCategoriaDto } from './dtos/atualizar-categoria-dto';
 require('dotenv').config({ path: '.env' })
 
 
@@ -18,9 +19,6 @@ export class AppController {
             options: {
                 urls: [process.env.RABBITMQ_URI],
                 queue: 'admin-backend',
-                queueOptions: {
-                  durable: false,
-                },
             }
         })
     }
@@ -35,8 +33,13 @@ export class AppController {
 
     @Get('categorias')
     consultaCategorias(@Query('id_categoria') _id : string): Observable<any>{
+        return this.clientAdminBackend.send('consultar-categorias', _id ? _id: '');        
+    }
 
-        return this.clientAdminBackend.send('consultar-categorias', _id ? _id: '')
-        
+    @Put('categorias/:_id')
+    @UsePipes(ValidationPipe)
+    atualizarCategoria(@Body() atualizarCategoriaDto: AtualizarCategoriaDto,
+    @Param('_id') _id : string){
+        this.clientAdminBackend.emit('atualizar-categoria',{id: _id, categoria: atualizarCategoriaDto});
     }
 }
