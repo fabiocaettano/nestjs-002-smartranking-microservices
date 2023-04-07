6d703728b83b3c5eb53177c4de6b62985f03b6aa
fabiocaettano <fabiocaettano74@gmail.com>
2023-04-07 21:49:06 +0000

    aula 45 - implementado a classe LoginInterceptor para canmonitorar o tempo de resposta de uma requisição

---
+++ api-gateway/src/interceptors/logging.interceptors.ts
@@ -0,0 +1,13 @@
+import { NestInterceptor, ExecutionContext , CallHandler} from "@nestjs/common";
+import { Observable } from "rxjs";
+import { tap } from "rxjs";
+
+export class LogginInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        console.log('Antes ...')
+        const newDate = Date.now()
+        return next.handle().pipe(
+            tap(() => console.log('Depois ... ${Date.now() - newDate } ms')),
+        )        
+    }
+}
