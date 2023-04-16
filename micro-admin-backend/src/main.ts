6fe98a8504ea6e84dc71403707bec345decc83ce
fabiocaettano <fabio.caettano74@gmail.com>
2023-04-16 12:37:33 +0000

    aula 52 -  refatorado para incluir a classe ConfigService do NestJs, para tratar variáveis de ambiente

--- micro-admin-backend/src/main.ts
+++ micro-admin-backend/src/main.ts
@@ -2,15 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
 import { Logger } from '@nestjs/common';
-require('dotenv').config({ path: '.env' });
+import { ConfigService } from '@nestjs/config';
+
+//require('dotenv').config({ path: '.env' });
+//urls: [process.env.RABBITMQ_URI],
 
 const logger = new Logger('Main');
+const configService = new ConfigService();
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
       transport: Transport.RMQ,
       options: {
-        urls: [process.env.RABBITMQ_URI],
+        urls: [configService.get<string>('RABBITMQ_URI')],
         noAck: false,
         queue: 'admin-backend',
       },
