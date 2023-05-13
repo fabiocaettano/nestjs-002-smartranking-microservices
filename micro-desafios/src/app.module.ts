52df4b1bd2f4e3d49407182c2cd999e6b53c5470
fabiocaettano <fabio.caettano74@gmail.com>
2023-05-13 16:58:46 +0000

    criado module, controller e service

--- micro-desafios/src/app.module.ts
+++ micro-desafios/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DesafiosModule } from './desafios/desafios.module';
 
 @Module({
-  imports: [],
+  imports: [DesafiosModule],
   controllers: [AppController],
   providers: [AppService],
 })
