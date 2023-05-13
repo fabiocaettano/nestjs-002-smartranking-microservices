2961ea8a37b2b4fd521a59f97c17a679df3db8cc
fabiocaettano <fabio.caettano74@gmail.com>
2023-05-13 17:00:41 +0000

    excluido app.controller e app.service

--- micro-desafios/src/app.module.ts
+++ micro-desafios/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DesafiosModule } from './desafios/desafios.module';
 
 @Module({
   imports: [DesafiosModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
